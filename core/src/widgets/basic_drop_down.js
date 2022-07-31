1216fd6e7ca6d5d5a892d5e6b0138a4fd81f9871
Kiveisha <yevgeniy.kiveisha@hp.com>
2022-07-31 15:34:04 +0300

    dropdown basic ui control
    Signed-off-by: Kiveisha <yevgeniy.kiveisha@hp.com>

--- core/src/widgets/basic_drop_down.js
+++ core/src/widgets/basic_drop_down.js
@@ -1,44 +1,78 @@
-function MksBasicDropDown () {
+function CoreUIBasicDropDown () {
 	self = this;
 
-    this.WorkingObject      = null;
-    this.Content            = `
-        <button class="btn btn-outline-secondary dropdown-toggle btn-sm" type="button" id="[HOST_ID]_content" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    this.WorkingObject  = null;
+    this.WidgetID       = null;
+    this.Content        = `
+        <div class="custom-control custom-radio xmt-3">
+            <select class="custom-select mt-3" id="[ID]_items">
+            </select>
+        </div>
+    `;
+    this.RowView = `<option value="[VALUE]" id="[ID]_item_[NAME]" onclick="[ONCLICK_HANDLER](this);">[NAME]</option>`;
+    /*
+    this.Content        = `
+        <button class="btn btn-outline-secondary dropdown-toggle btn-sm" type="button" id="[ID]_content" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span data-feather="calendar"></span>
-            <span id="[HOST_ID]_name"></span>
+            <span id="[ID]_name"></span>
         </button>
-        <div class="dropdown-menu text-center" aria-labelledby="[HOST_ID]_content" id="[HOST_ID]_items"></div>
+        <div class="dropdown-menu text-center" aria-labelledby="[ID]_content" id="[ID]_items"></div>
     `;
     this.RowView = `<span class="dropdown-item" style="cursor:pointer" onclick="[ONCLICK_HANDLER]">[NAME]</span>`;
+    */
 	
 	return this;
 }
 
-MksBasicDropDown.prototype.UpdateSelected = function (name) {
-    var obj = document.getElementById(this.WorkingObject.id + "_name");
-    obj.innerHTML = name;
-}
+CoreUIBasicDropDown.prototype.Build = function (id) {
+    this.WorkingObject  = document.getElementById(id);
+    this.WidgetID       = id+"_core_ui_basic_drop_down";
+    var html            = this.Content;
 
-MksBasicDropDown.prototype.AppendItem = function (item) {
-    var obj = document.getElementById(this.WorkingObject.id + "_items");
-    var html = this.RowView;
-    html = html.split("[ONCLICK_HANDLER]").join(item.onclick);
-    html = html.split("[NAME]").join(item.name);
-    obj.innerHTML += html;
-}
-
-MksBasicDropDown.prototype.Build = function (obj) {
-    this.WorkingObject = obj;
-    var html = this.Content;
-
-    var HostingId = this.WorkingObject.id;
-    html = html.split("[HOST_ID]").join(HostingId);
-    obj.innerHTML = html;
+    // Update content with user info
+    html = html.split("[ID]").join(this.WidgetID);
+    // Set HTML
+    this.WorkingObject.innerHTML = html;
     feather.replace();
 }
 
-MksBasicDropDown.prototype.Remove = function () {
+CoreUIBasicDropDown.prototype.Remove = function () {
     if (this.WorkingObject !== undefined && this.WorkingObject !== null) {
 		this.WorkingObject.parentNode.removeChild(this.WorkingObject);
 	}
+}
+
+CoreUIBasicDropDown.prototype.Show = function () {
+    document.getElementById(this.WidgetID).classList.remove("d-none");
+}
+
+CoreUIBasicDropDown.prototype.Hide = function () {
+    document.getElementById(this.WidgetID).classList.add("d-none");
+}
+
+CoreUIBasicDropDown.prototype.GetHtml = function () {
+    return this.WorkingObject.innerHTML;
+}
+
+CoreUIBasicDropDown.prototype.UpdateSelected = function (name) {
+    var obj = document.getElementById(this.WidgetID+"_item_"+name);
+    obj.selected = true;
+}
+
+CoreUIBasicDropDown.prototype.AppendItem = function (item) {
+    var obj = document.getElementById(this.WidgetID + "_items");
+    var html = this.RowView;
+    html = html.split("[ID]").join(this.WidgetID);
+    html = html.split("[ONCLICK_HANDLER]").join(item.onclick);
+    html = html.split("[NAME]").join(item.name);
+    html = html.split("[VALUE]").join(item.value);
+    obj.innerHTML += html;
+}
+
+CoreUIBasicDropDown.prototype.GetSelected = function() {
+    var select = document.getElementById(this.WidgetID + "_items")
+    return {
+        value: select.value,
+        name: select.options[select.selectedIndex].text
+    }
 }
